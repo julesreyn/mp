383ed6ad2d7627cc621f8b671b5c3d9e1fe663c0
julesreyn <jules.reynaud@epitech.eu>
2024-03-22 17:06:58 +0100

    Update config_init.sh script for nvm install

--- config_init.sh
+++ config_init.sh
@@ -9,9 +9,9 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 
 export NVM_DIR='$HOME/.nvm'; [ -s '$NVM_DIR/nvm.sh' ] && \. '$NVM_DIR/nvm.sh'; [ -s '$NVM_DIR/bash_completion' ] && \. '$NVM_DIR/bash_completion'
 
-nvm install 20
-
 sudo apt-get update
+
+nvm install 20
 sudo apt-get install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
