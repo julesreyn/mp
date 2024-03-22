346420f7edb67948091b6a330c0422071b7874bc
julesreyn <jules.reynaud@epitech.eu>
2024-03-22 10:02:22 +0100

    Add multipass python lib

---
+++ init-vm.py
@@ -0,0 +1,71 @@
+##
+## Conseil Junior Taker - 2024
+## mp [Ubuntu]
+## File description:
+## multipass library
+## @julesreyn
+##
+
+from multipass import *
+import secrets
+import string
+
+INSTANCE_IMAGE = "22.04"
+INSTANCE_CPUS = "1"
+INSTANCE_MEMORY = "2G"
+
+def instance_name_gen():
+    return ''.join(secrets.choice(string.ascii_lowercase + string.digits) for _ in range(6))
+
+# def init_cloudflare_service():
+    #TO-DO
+
+# def update_motd(name):
+    # TO-DO
+
+def install_prerequisites(name):
+    exec_command(name, "sudo apt update -y && sudo apt upgrade -y")
+
+    ## Install Python3, Python3-pip
+    exec_command(name, "sudo apt install  python3 python3-pip -y")
+
+    ## Install NodeJS, NVM
+    exec_command(name, "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash")
+    exec_command("""
+        export NVM_DIR="$HOME/.nvm"
+        [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+        [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+        """)
+    exec_command(name, "nvm install 20")
+
+    ## Install Docker and Docker Compose
+    exec_command(name, """
+            sudo apt-get update
+            sudo apt-get install ca-certificates curl
+            sudo install -m 0755 -d /etc/apt/keyrings
+            sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+            sudo chmod a+r /etc/apt/keyrings/docker.asc
+            echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+            sudo apt-get update
+        """)
+    exec_command(name, "sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y")
+    exec_command(name, "sudo curl -L 'https://github.com/docker/compose/releases/download/2.25.0/docker-compose-$(uname -s)-$(uname -m)' -o /usr/local/bin/docker-compose")
+    exec_command(name, "sudo chattr +i /etc/hostname")
+    exec_command(name, "sudo apt update -y && sudo apt upgrade -y")
+
+    ## Install Cloudflare service
+    exec_command(name, "sudo mkdir -p --mode=0755 /usr/share/keyrings")
+    exec_command(name, "curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | sudo tee /usr/share/keyrings/cloudflare-main.gpg >/dev/null")
+    exec_command(name, "echo 'deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared $(lsb_release -cs) main' | sudo tee /etc/apt/sources.list.d/cloudflared.list")
+    exec_command(name, "sudo apt-get update -y && sudo apt-get install cloudflared -y")
+    put_file(name, "/home/jules/.cloudflared/cert.pem", "/home/ubuntu/.cloudflared/cert.pem")
+
+def init_instance(image=INSTANCE_IMAGE, cpu=INSTANCE_CPUS, memory=INSTANCE_MEMORY):
+    name = instance_name_gen()
+    launch_instance(name, image, cpu, memory)
+    install_prerequisites(name)
+    return name
+
+
+if __name__ == "__main__":
+    init_instance()
---
+++ multipass.py
@@ -0,0 +1,73 @@
+##
+## Conseil Junior Taker - 2024
+## mp [Ubuntu]
+## File description:
+## multipass library
+## @julesreyn
+##
+
+import os
+
+def launch_instance(name="default_name", image="22.04", cpus="1", memory="2G"):
+    os.system("multipass launch --name " + name + " --cpus " + cpus + " --memory " + memory + " " + image)
+
+def list_instances():
+    os.system("multipass list")
+
+def stop_instance(name):
+    os.system("multipass stop " + name)
+
+def start_instance(name):
+    os.system("multipass start " + name)
+
+def delete_instance(name):
+    os.system("multipass delete " + name + " --purge")
+
+def delete_all_instances():
+    os.system("multipass delete --all --purge")
+
+def exec_command(name, command):
+    os.system("multipass exec " + name + " -- " + command)
+
+def get_ip(name):
+    os.system("multipass info " + name + " | grep IPv4 | awk '{print $2}'")
+
+def get_state(name):
+    os.system("multipass info " + name + " | grep State | awk '{print $2}'")
+
+def get_image(name):
+    os.system("multipass info " + name + " | grep Image | awk '{print $2}'")
+
+def put_file(name, source, destination):
+    os.system("multipass transfer " + source + " " + name + ":" + destination)
+
+def get_file(name, source, destination):
+    os.system("multipass transfer " + name + ":" + source + " " + destination)
+
+def mount(name, source, destination):
+    os.system("multipass mount " + source + " " + name + ":" + destination)
+
+def unmount(name):
+    os.system("multipass unmount " + name)
+
+def get_cpu_usage(name):
+    os.system("multipass exec " + name + " -- mpstat 1 1 | grep all | awk '{print $3}'")
+
+def get_memory_usage(name):
+    os.system("multipass exec " + name + " -- free -m | grep Mem | awk '{print $3}'")
+
+def get_disk_usage(name):
+    os.system("multipass exec " + name + " -- df -h | grep /dev/root | awk '{print $3}'")
+
+def get_uptime(name):
+    os.system("multipass exec " + name + " -- uptime | awk '{print $3}'")
+
+def get_processes(name):
+    os.system("multipass exec " + name + " -- ps aux | wc -l")
+
+def get_nb_users(name):
+    os.system("multipass exec " + name + " -- who | wc -l")
+
+def get_hostname(name):
+    os.system("multipass exec " + name + " -- hostname")
+
