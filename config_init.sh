5fc40b6ba2a013c6fec667b0083712b5315b633d
julesreyn <jules.reynaud@epitech.eu>
2024-03-22 17:19:36 +0100

    Fix env reloading for nvm install

--- config_init.sh
+++ config_init.sh
@@ -13,7 +13,9 @@ echo -e "\n\n[+] Installing nvm & nodejs v20:lts\n\n"
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 
-export NVM_DIR='$HOME/.nvm'; [ -s '$NVM_DIR/nvm.sh' ] && \. '$NVM_DIR/nvm.sh'; [ -s '$NVM_DIR/bash_completion' ] && \. '$NVM_DIR/bash_completion'
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 sudo apt-get update
 nvm install 20
