8eaa9b3f8d42b278cb2aea8692aba301fa90f629
julesreyn <jules.reynaud@epitech.eu>
2024-03-26 12:31:06 +0100

    Add logging and improve configuration process

--- taker_lib/cmd/instance_prerequisites.py
+++ taker_lib/cmd/instance_prerequisites.py
@@ -9,6 +9,9 @@
 from taker_lib.cmd.instance_operations import instance_name_gen, exec_command
 from taker_lib.cmd.instance_operations import launch_instance
 from taker_lib.cmd.file_operations import put_file
+import	logging
+
+log = logging.getLogger(__name__)
 
 DEFAULT_INSTANCE_IMAGE = "22.04" # available options: 20.04, 22.04, 23.10, daily:24.04, docker, jellyfin, core, core18, core20, core22  -> search new image with "multipass find"
 DEFAULT_INSTANCE_VCPUS = "1" # available options: 1, 2, 4, 6
@@ -27,10 +30,13 @@ def upload_config(name):
     Example:
         >>> upload_config("instance_name")
     """
+    log.info(f'Uploading configuration files to instance {name}')
+    log.info(f'Uploading config.sh to instance {name}')
     put_file(name, "./setup_tools/config.sh", "config.sh")
+    log.info(f'Uploading update-motd.d/00-header to instance {name}')
     put_file(name, "./setup_tools/update-motd.d/00-header", "00-header")
+    log.info(f'Uploading update-motd.d/10-help-text to instance {name}')
     put_file(name, "./setup_tools/update-motd.d/10-help-text", "10-help-text")
-
 
 
 def install_prerequisites(name):
@@ -46,14 +52,19 @@ def install_prerequisites(name):
     Example:
         >>> install_prerequisites("instance_name")
     """
+    log.info(f'Installing prerequisites on instance {name}')
     upload_config(name)
+    log.info(f'Adding execution permissions to config.sh on instance {name}')
+    exec_command(name, "chmod +x /ome/ubuntu/config.sh")
+    log.info(f'Executing config.sh on instance {name}')
     exec_command(name, "bash /home/ubuntu/config.sh")
+    log.info(f'Removing config.sh from instance {name}')
     exec_command(name, "rm /home/ubuntu/config.sh")
+    log.info(f'Uploading cloudflared certificat to instance {name}')
     put_file(name, "~/.cloudflared/cert.pem", "~/.cloudflared/cert.pem")
 
 
-
-def init_instance(image=DEFAULT_INSTANCE_IMAGE, cpu=DEFAULT_INSTANCE_VCPUS, memory=DEFAULT_INSTANCE_MEMORY):
+def init_instance(image=DEFAULT_INSTANCE_IMAGE, cpu=DEFAULT_INSTANCE_VCPUS, memory=DEFAULT_INSTANCE_MEMORY, config=True):
     """
     Initializes a new instance
 
@@ -61,6 +72,7 @@ def init_instance(image=DEFAULT_INSTANCE_IMAGE, cpu=DEFAULT_INSTANCE_VCPUS, memo
         image (str): The image of the instance, default is "22.04"
         cpu (str): The number of CPUs, default is "1"
         memory (str): The amount of memory, default is "2G"
+        config (bool): Whether to configure the instance after launching, default is True
 
     Returns:
         str: The name of the instance
@@ -71,5 +83,7 @@ def init_instance(image=DEFAULT_INSTANCE_IMAGE, cpu=DEFAULT_INSTANCE_VCPUS, memo
     """
     name = instance_name_gen()
     launch_instance(name, image, cpu, memory)
-    install_prerequisites(name)
+    if config:
+        log.info(f'Configuring instance {name} with Taker requirements')
+        install_prerequisites(name)
     return name
