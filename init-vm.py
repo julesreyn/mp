000c97d6f841fd63307fe1a2744807b66a7965bd
julesreyn <jules.reynaud@epitech.eu>
2024-04-22 12:08:13 +0200

    Refactor init-vm.py and update instance initialization

--- init-vm.py
+++ init-vm.py
@@ -16,6 +16,4 @@ if __name__ == "__main__":
     logging.basicConfig(filename=f"logs/instances/init-vm-{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log",
                         level=logging.INFO,
                         format='%(asctime)s - %(levelname)s - %(message)s')
-    log.info("Checking for server prerequisites")
-    log.info("Starting instance initialization")
     init_instance()
